16fd31fe4af83c231e239850b8539b63f4d22137
LevNakon <lionsoul887@gmail.com>
2020-03-02 16:09:36 +0200

    DB configuration

---
+++ Event.io/Models/EventContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Event.io.Models
+{
+    public class EventContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Semester> Semesters { get; set; }
+
+        public EventContext(DbContextOptions<EventContext> options) 
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
